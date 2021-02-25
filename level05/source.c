e1ae0728bf653ed9dc7deb295b0715128260f489
Valentine-mooder <valentine@supermood.fr>
2021-02-25 22:28:24 +0100

    add source code

---
+++ level05/source.c
@@ -0,0 +1,20 @@
+int	main(int argc,char **argv)
+{
+	int i= 0;
+	char str[100];
+
+	fgets( str, 100, stdin);
+while (strlen(str) < i)
+{
+if ( str[i] > 64)
+{
+			if (str[i] <= 90)
+			{
+				lowercase(str[i]);
+}
+i++;
+}
+}
+	exit(0);
+}
+
